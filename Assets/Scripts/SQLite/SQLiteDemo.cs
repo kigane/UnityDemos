3a095984ca9baa8974a00c4e92d8880508660630
kigane <huwkigane@outlook.com>
2023-02-07 20:03:40 +0800

    SQLite的基础使用

---
+++ Assets/Scripts/SQLite/SQLiteDemo.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System;
+
+namespace Demos
+{
+    public class SQLiteDemo : MonoBehaviour
+    {
+        string dbName = "Assets/Scripts/SQLite/TestDB.db";
+        string ConnectionString => $"Data Source={dbName}";
+
+        private void Start()
+        {
+            // 创建Table
+            CreateDB();
+
+            // 添加数据
+            AddPerson(1, "Hello", "World");
+
+            // 查询数据
+            QueryAllPerson();
+        }
+
+        private void CreateDB()
+        {
+            using (var connection = new SqliteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "CREATE TABLE Person (Id INTEGER NOT NULL UNIQUE, FirstName TEXT NOT NULL, SecondName TEXT NOT NULL,PRIMARY KEY(Id AUTOINCREMENT));";
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void AddPerson(int id, string firstName, string secondName)
+        {
+            using (var connection = new SqliteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = $"INSERT INTO Person (Id, FirstName, SecondName) VALUES (null, '{firstName}', '{secondName}');";
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void QueryAllPerson()
+        {
+            using (var connection = new SqliteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Person";
+                    using (IDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Log.Debug($"{reader["Id"]}: {reader["FirstName"]}-{reader["SecondName"]}");
+                        }
+                        reader.Close();
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
